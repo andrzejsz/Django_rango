dd5b533bfd57d4107f3eee017d01f80e1b64ccce
Andrzej Szczygielski <andrzejsz@gmail.com>
2014-08-02 19:09:27 +0100

    media server

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,10 @@ urlpatterns = patterns('',
 
     url(r'^rango/', include('rango.urls')), # mapping for rango app
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}),)
