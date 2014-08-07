91bf7daa4cba18b662032b25b82c3c6ed7b2b6ec
Andrzej Szczygielski <andrzejsz@gmail.com>
2014-08-07 22:24:31 +0100

    about template page

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,7 @@ def index(request):
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango says: Here is about page <a href='/rango/'>Index</a>")
+    context = RequestContext(request)
+    context_dict = {'italicmessage': 'This is italic message'}
+    return render_to_response('rango/about.html', context_dict, context)
+
